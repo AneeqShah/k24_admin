12f8c7a4b9984d33a45cc9c26a47665df42db366
aneeqshah <aneeqshah15@gmail.com>
2022-10-02 00:08:07 +0500

    history done

--- lib/presentation/views/order_history/layout/order_history_body.dart
+++ lib/presentation/views/order_history/layout/order_history_body.dart
@@ -1,19 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+import 'package:k24_admin/app/custom_loader.dart';
 import 'package:k24_admin/presentation/views/order_history/layout/widgets/history_tile.dart';
 
-class OrderHistoryBody extends StatelessWidget {
+class OrderHistoryBody extends StatefulWidget {
   const OrderHistoryBody({Key? key}) : super(key: key);
 
   @override
+  State<OrderHistoryBody> createState() => _OrderHistoryBodyState();
+}
+
+class _OrderHistoryBodyState extends State<OrderHistoryBody> {
+  List<DocumentSnapshot> allHistory = [];
+  List<DocumentSnapshot> allUser = [];
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _getHistory();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-     height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: ListView.builder(
-          itemCount: 5,
-          itemBuilder: (context,i){
-        return HistoryTile();
-      }),
+    return CustomLoader(
+      isLoading: isLoading,
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: ListView.builder(
+            itemCount: allHistory.length,
+            itemBuilder: (context, i) {
+              DateTime time = DateTime.fromMillisecondsSinceEpoch(
+                  allHistory[i]["orderTime"]);
+              var uploadTime = Jiffy(time).yMMMd;
+              return HistoryTile(
+                  image: allUser[i]["image"],
+                  userName: allUser[i]["name"],
+                  price: allHistory[i]["price"],
+                  message: allHistory[i]["askedQuestion"],
+                  date: uploadTime);
+            }),
+      ),
     );
+  }
+
+  _getHistory() async {
+    await FirebaseFirestore.instance
+        .collection("order")
+        .orderBy("orderTime", descending: true)
+        .snapshots()
+        .listen((QuerySnapshot snapshot) async {
+      allHistory.clear();
+      snapshot.docs.forEach((element) async {
+        allHistory.add(element);
+        var a = await FirebaseFirestore.instance
+            .collection("Users")
+            .doc(element["customerID"])
+            .get();
+        allUser.add(a);
+        setState(() {});
+      });
+    });
+  }
+
+  loadingTrue() {
+    isLoading = true;
+    setState(() {});
+  }
+
+  loadingFalse() {
+    isLoading = false;
+    setState(() {});
   }
 }
--- lib/presentation/views/order_history/layout/widgets/history_tile.dart
+++ lib/presentation/views/order_history/layout/widgets/history_tile.dart
@@ -1,37 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:k24_admin/presentation/elements/Custom_image_container.dart';
 import 'package:k24_admin/presentation/elements/custom_text.dart';
 
 class HistoryTile extends StatelessWidget {
-  const HistoryTile({Key? key}) : super(key: key);
+  final String image;
+  final String userName;
+  final String price;
+  final String message;
+  final String date;
+
+  const HistoryTile(
+      {super.key,
+      required this.image,
+      required this.userName,
+      required this.price,
+      required this.message,
+      required this.date});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(
-        radius: 35,
-        backgroundImage: AssetImage('assets/images/dp.png'),
-      ),
+      leading: CustomImageContainer(
+          height: 50, wight: 50, radius: 500, image: image),
       title: Padding(
         padding: const EdgeInsets.only(top: 12.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            CustomText(text: 'Luna'),
-            CustomText(text: '€ 2'),
+            CustomText(text: userName),
+            CustomText(text: '€ ${price}'),
           ],
         ),
       ),
-      subtitle: Column(crossAxisAlignment: CrossAxisAlignment.start,
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          SizedBox(height: 5,),
+          SizedBox(
+            height: 5,
+          ),
           CustomText(
-              text: 'Lorem Ipsum is simply dummy text of the printing'
-                  'and typesetting industry. Lorem Ipsum ',
-          fontWeight: FontWeight.w300,
+            text: message,
+            fontWeight: FontWeight.w300,
             fontSize: 11,
           ),
-          SizedBox(height: 5,),
-          CustomText(text: 'Date 21/12/2022',fontSize: 12,fontWeight: FontWeight.w500,),
+          SizedBox(
+            height: 5,
+          ),
+          CustomText(
+            text: 'Date ${date}',
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+          ),
           Divider(),
         ],
       ),
